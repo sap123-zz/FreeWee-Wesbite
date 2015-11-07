5479947a5e9d2e0eff67872d8a630e531c572c67
Suraj Palwe <surajpalwe13@gmail.com>
2015-11-07 21:35:40 +0530

    added different file to get url for function

--- main.py
+++ main.py
@@ -1,32 +1,20 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,redirect
 app = Flask(__name__)
 
-import requests
-import wget
-from bs4 import BeautifulSoup
+from DownloadMethods import DownloadPic
 
 url_extensions = ['.jpg','.png']
-
-def downloadFile(url):
-    html_text = requests.get(url)
-    soup = BeautifulSoup(html_text.text)
-    for meta in soup.find_all('meta'):
-        proper = str(meta.get('property'))
-        if proper == 'og:image':
-            picUrl = str(meta.get('content'))
-            #print picUrl
-            filename = wget.download(picUrl)
-            #print 'download done'
 
 @app.route('/',methods=['GET','POST'])
 def index():
     notication = ""
     if request.method=='POST':
         url = str(request.form['url'])
-        downloadFile(url)
+        downloadUrl = DownloadPic(url)
         notification = 'Download Complete'
-        return render_template('thanks.html',notification=notification)
-    return render_template('index.html')
+        
+    if request.method == 'GET':
+        return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
