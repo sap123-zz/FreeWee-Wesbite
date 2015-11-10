b700499e2fa58e72919a1d480df64adb02f7b377
Suraj Palwe <surajpalwe13@gmail.com>
2015-11-10 10:56:43 +0530

    removed unused imports and comments, : cleanUP code

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python
-import os
 
 from main import app as application
-
-# IMPORTANT: Put any additional includes below this line.  If placed above this
-# line, it's possible required libraries won't be in your searchable path
-#
 
