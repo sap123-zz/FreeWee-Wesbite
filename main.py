88a3e41bfd1c441ea1ee538a2b3732b825a760a0
Suraj Palwe <surajpalwe13@gmail.com>
2015-11-16 10:22:47 +0530

    file to handle backend

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
-from flask import Flask
-
+from flask import Flask,render_template,request,redirect
 app = Flask(__name__)
 
-@app.route('/')
+from DownloadMethods import DownloadPic
+from Platform import IS_OPENSHIFT
+
+#url_extensions = ['.jpg','.png']
+
+@app.route('/',methods=['GET','POST'])
 def index():
-    return 'hello world'
+    notication = ""
+    if request.method=='POST':
+        url = str(request.form['url'])
+        downloadUrl = DownloadPic(url)
+        return render_template('index.html', downloadUrl = downloadUrl)
+        
+    if request.method == 'GET':
+        return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run()
+    if IS_OPENSHIFT:
+        app.run()
+    else:
+        app.run(debug = True)
+
