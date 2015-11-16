bdd9f044cddc257108b9524102f1d896cbdd1b45
Suraj Palwe <surajpalwe13@gmail.com>
2015-11-16 12:47:02 +0530

    test #10

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 from flask import Flask,render_template,request,redirect
 app = Flask(__name__)
 
-import requests
-from bs4 import BeautifulSoup
-#from DownloadMethods import DownloadPic
+from DownloadMethods import DownloadPic
 from Platform import IS_OPENSHIFT
 
 #url_extensions = ['.jpg','.png']
 
 @app.route('/',methods=['GET','POST'])
 def index():
-    return 'hello'
+    notication = ""
+    if request.method=='POST':
+        url = str(request.form['url'])
+        downloadUrl = DownloadPic(url)
+        return render_template('index.html', downloadUrl = downloadUrl)
+        
+    if request.method == 'GET':
+        return render_template('index.html')
+
 if __name__ == '__main__':
     if IS_OPENSHIFT:
         app.run()
