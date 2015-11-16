f65a253a41b8b4f236464c4b82bd86935db1c550
Suraj Palwe <surajpalwe13@gmail.com>
2015-11-16 11:18:28 +0530

    test #5

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ app = Flask(__name__)
 
 #from DownloadMethods import DownloadPic
 from Platform import IS_OPENSHIFT
+from bs4 import BeautifulSoup
 
 #url_extensions = ['.jpg','.png']
 
 @app.route('/',methods=['GET','POST'])
 def index():
     return 'hello'
+
 if __name__ == '__main__':
   if IS_OPENSHIFT:
     app.run()
