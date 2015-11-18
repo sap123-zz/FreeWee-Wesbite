66dd3625a11fdbc7beb71a00e9b28ae00aec7bce
Suraj Palwe <surajpalwe13@gmail.com>
2015-11-18 17:06:44 +0530

    file is dowloaded to filesystem and named.

--- DownloadMethods.py
+++ DownloadMethods.py
@@ -1,13 +1,32 @@
 # this file is to write functions to download from various sites
 import requests
+import datetime
 from bs4 import BeautifulSoup
 
+def GetFileExtension(URL):
+  formats = ['.jpg','.jpeg','.png']
+  for i in formats:
+    if (i in URL):
+      return i
+
+def CreateFileName(picUrl):
+  timenowInMilli = str(datetime.datetime.now().strftime("%s"))
+  extension = GetFileExtension(picUrl)
+  nameOfImage = 'static/images/'+timenowInMilli + extension
+  return str(nameOfImage)
+
+
 def DownloadPic(url):
+    imgDict = {}
     htmlText = requests.get(url)
     soup = BeautifulSoup(htmlText.text)
     for meta in soup.find_all('meta'):
         _property = str(meta.get('property'))
         if _property == 'og:image':
             picUrl = str(meta.get('content'))
-            return picUrl
+            nameOfImage = CreateFileName(picUrl)
+            fileName = open(nameOfImage,'wb')
+            fileName.write(requests.get(picUrl).content)
+            imgDict = {'picURL': picUrl,'filelocation':nameOfImage}
+            return imgDict
 
--- main.py
+++ main.py
@@ -4,18 +4,16 @@ app = Flask(__name__)
 from DownloadMethods import DownloadPic
 from Platform import IS_OPENSHIFT
 
-#url_extensions = ['.jpg','.png']
-
 @app.route('/',methods=['GET','POST'])
 def index():
-    notication = ""
     if request.method=='POST':
         url = str(request.form['url'])
         downloadUrl = DownloadPic(url)
-        return render_template('index.html', downloadUrl = downloadUrl)
+        return render_template('index.html', downloadUrl = downloadUrl['picURL'])
         
     if request.method == 'GET':
         return render_template('index.html')
+
 
 if __name__ == '__main__':
     if IS_OPENSHIFT:
