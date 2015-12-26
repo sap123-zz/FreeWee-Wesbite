46133cf63d0a91d6222031f06e1241ce91117a1e
Suraj Palwe <surajpalwe13@gmail.com>
2015-12-27 00:23:57 +0530

    updated the api, added artwork_url in default data of Song

--- TestProto.py
+++ TestProto.py
@@ -3,55 +3,65 @@
 songData = {
           "result": [
                   {
-                            "id": 229615899,
-                                  "stream_url": "https://api.soundcloud.com/tracks/229615899/stream",
-                                        "title": "Tum Sath Ho- Arjit Singh - Tamasha 2015"
-                                            },
+                            "artwork_url": "https://i1.sndcdn.com/artworks-000133604698-5y7azu-large.jpg",
+                                  "id": 229615899,
+                                        "stream_url": "https://api.soundcloud.com/tracks/229615899/stream",
+                                              "title": "Tum Sath Ho- Arjit Singh - Tamasha 2015"
+                                                  },
                       {
-                                "id": 168257758,
-                                      "stream_url": "https://api.soundcloud.com/tracks/168257758/stream",
-                                            "title": "Sajjad Ali, Tum Naraz Ho, Coke Studio Season 7, Episode 1."
-                                                },
+                                "artwork_url": "https://i1.sndcdn.com/artworks-000091577682-rhkzah-large.jpg",
+                                      "id": 168257758,
+                                            "stream_url": "https://api.soundcloud.com/tracks/168257758/stream",
+                                                  "title": "Sajjad Ali, Tum Naraz Ho, Coke Studio Season 7, Episode 1."
+                                                      },
                           {
-                                    "id": 229180073,
-                                          "stream_url": "https://api.soundcloud.com/tracks/229180073/stream",
-                                                "title": "Tum Sath Ho"
-                                                    },
+                                    "artwork_url": "https://i1.sndcdn.com/artworks-000133287078-7icve0-large.jpg",
+                                          "id": 229180073,
+                                                "stream_url": "https://api.soundcloud.com/tracks/229180073/stream",
+                                                      "title": "Tum Sath Ho"
+                                                          },
                               {
-                                        "id": 154550407,
-                                              "stream_url": "https://api.soundcloud.com/tracks/154550407/stream",
-                                                    "title": "Tum Hi Ho  ashiqui2 songs  -the best song  in 2014-"
-                                                        },
+                                        "artwork_url": "https://i1.sndcdn.com/artworks-000082488007-v1lvs3-large.jpg",
+                                              "id": 154550407,
+                                                    "stream_url": "https://api.soundcloud.com/tracks/154550407/stream",
+                                                          "title": "Tum Hi Ho  ashiqui2 songs  -the best song  in 2014-"
+                                                              },
                                   {
-                                            "id": 36347117,
-                                                  "stream_url": "https://api.soundcloud.com/tracks/36347117/stream",
-                                                        "title": "Tum ek Gorakh Dhanda Ho - Nusrat Fateh Ali Khan (30:22)"
-                                                            },
+                                            "artwork_url": "https://i1.sndcdn.com/artworks-000018227052-6589q6-large.jpg",
+                                                  "id": 36347117,
+                                                        "stream_url": "https://api.soundcloud.com/tracks/36347117/stream",
+                                                              "title": "Tum ek Gorakh Dhanda Ho - Nusrat Fateh Ali Khan (30:22)"
+                                                                  },
                                       {
-                                                "id": 228655140,
-                                                      "stream_url": "https://api.soundcloud.com/tracks/228655140/stream",
-                                                            "title": "Tum Saath Ho - Arijit Singh | Tamasha Movie"
-                                                                },
+                                                "artwork_url": "https://i1.sndcdn.com/artworks-000045273859-9ntpzg-large.jpg",
+                                                      "id": 87458429,
+                                                            "stream_url": "https://api.soundcloud.com/tracks/87458429/stream",
+                                                                  "title": "Tum Hi Ho ashiqui 2 female version"
+                                                                      },
                                           {
-                                                    "id": 87458429,
-                                                          "stream_url": "https://api.soundcloud.com/tracks/87458429/stream",
-                                                                "title": "Tum Hi Ho ashiqui 2 female version"
-                                                                    },
+                                                    "artwork_url": "https://i1.sndcdn.com/artworks-000132907737-rf059b-large.jpg",
+                                                          "id": 228655140,
+                                                                "stream_url": "https://api.soundcloud.com/tracks/228655140/stream",
+                                                                      "title": "Tum Saath Ho - Arijit Singh | Tamasha Movie"
+                                                                          },
                                               {
-                                                        "id": 228667006,
-                                                              "stream_url": "https://api.soundcloud.com/tracks/228667006/stream",
-                                                                    "title": "Agar Tum Saath Ho - Tamasha -"
-                                                                        },
+                                                        "artwork_url": "https://i1.sndcdn.com/artworks-000132916310-vg9mbu-large.jpg",
+                                                              "id": 228667006,
+                                                                    "stream_url": "https://api.soundcloud.com/tracks/228667006/stream",
+                                                                          "title": "Agar Tum Saath Ho - Tamasha -"
+                                                                              },
                                                   {
-                                                            "id": 16263738,
-                                                                  "stream_url": "https://api.soundcloud.com/tracks/16263738/stream",
-                                                                        "title": "Jagjit Singh-Tum Itna Jo Muskura Rahe Ho"
-                                                                            },
+                                                            "artwork_url": null,
+                                                                  "id": 16263738,
+                                                                        "stream_url": "https://api.soundcloud.com/tracks/16263738/stream",
+                                                                              "title": "Jagjit Singh-Tum Itna Jo Muskura Rahe Ho"
+                                                                                  },
                                                       {
-                                                                "id": 228700857,
-                                                                      "stream_url": "https://api.soundcloud.com/tracks/228700857/stream",
-                                                                            "title": "Urdu song-Agar tum sath ho..."
-                                                                                }
+                                                                    "artwork_url": null,
+                                                                          "id": 228700857,
+                                                                                "stream_url": "https://api.soundcloud.com/tracks/228700857/stream",
+                                                                                      "title": "Urdu song-Agar tum sath ho..."
+                                                                                          }
                                                         ]
 }
 
