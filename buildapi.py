49937ccdfb95d9e93bd20f91ce514a8319cf486a
Suraj Palwe <surajpalwe13@gmail.com>
2015-12-27 00:22:47 +0530

    added artwork_url to api

--- buildapi.py
+++ buildapi.py
@@ -4,6 +4,7 @@ import soundcloud
 client_id = 'eef823eb72081eccc8684bc619021062'
 
 def BuildJsonApi(tracks,error):
+#the json given by soundcloud is decoded into required format
     json_array  = []
     if error != "":
         return error
@@ -14,11 +15,13 @@ def BuildJsonApi(tracks,error):
                     "id"         : items.id,
                     "title"      : items.title,
                     "stream_url" : items.stream_url,
+                    "artwork_url": items.artwork_url
                 })
         return json_array
 
 
 def BuildApi(query):
+#gets the name of song searched and builds the tracks json
     client = soundcloud.Client(client_id = client_id)
     try :
         tracks = client.get('/tracks', q=str(query))
