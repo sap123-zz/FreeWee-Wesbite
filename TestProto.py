81941f1df4c814cbc9e5cefa926ad3194c9623d2
Suraj Palwe <surajpalwe13@gmail.com>
2015-12-26 23:30:22 +0530

    file contains all default data to test on all platforms

---
+++ TestProto.py
@@ -0,0 +1,58 @@
+#this file holds all the default data
+
+songData = {
+          "result": [
+                  {
+                            "id": 229615899,
+                                  "stream_url": "https://api.soundcloud.com/tracks/229615899/stream",
+                                        "title": "Tum Sath Ho- Arjit Singh - Tamasha 2015"
+                                            },
+                      {
+                                "id": 168257758,
+                                      "stream_url": "https://api.soundcloud.com/tracks/168257758/stream",
+                                            "title": "Sajjad Ali, Tum Naraz Ho, Coke Studio Season 7, Episode 1."
+                                                },
+                          {
+                                    "id": 229180073,
+                                          "stream_url": "https://api.soundcloud.com/tracks/229180073/stream",
+                                                "title": "Tum Sath Ho"
+                                                    },
+                              {
+                                        "id": 154550407,
+                                              "stream_url": "https://api.soundcloud.com/tracks/154550407/stream",
+                                                    "title": "Tum Hi Ho  ashiqui2 songs  -the best song  in 2014-"
+                                                        },
+                                  {
+                                            "id": 36347117,
+                                                  "stream_url": "https://api.soundcloud.com/tracks/36347117/stream",
+                                                        "title": "Tum ek Gorakh Dhanda Ho - Nusrat Fateh Ali Khan (30:22)"
+                                                            },
+                                      {
+                                                "id": 228655140,
+                                                      "stream_url": "https://api.soundcloud.com/tracks/228655140/stream",
+                                                            "title": "Tum Saath Ho - Arijit Singh | Tamasha Movie"
+                                                                },
+                                          {
+                                                    "id": 87458429,
+                                                          "stream_url": "https://api.soundcloud.com/tracks/87458429/stream",
+                                                                "title": "Tum Hi Ho ashiqui 2 female version"
+                                                                    },
+                                              {
+                                                        "id": 228667006,
+                                                              "stream_url": "https://api.soundcloud.com/tracks/228667006/stream",
+                                                                    "title": "Agar Tum Saath Ho - Tamasha -"
+                                                                        },
+                                                  {
+                                                            "id": 16263738,
+                                                                  "stream_url": "https://api.soundcloud.com/tracks/16263738/stream",
+                                                                        "title": "Jagjit Singh-Tum Itna Jo Muskura Rahe Ho"
+                                                                            },
+                                                      {
+                                                                "id": 228700857,
+                                                                      "stream_url": "https://api.soundcloud.com/tracks/228700857/stream",
+                                                                            "title": "Urdu song-Agar tum sath ho..."
+                                                                                }
+                                                        ]
+}
+
+#print songData
