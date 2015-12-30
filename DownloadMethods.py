7d78c65bb8897a55ddf1ac722da764e8ed726ae4
Suraj Palwe <surajpalwe13@gmail.com>
2015-12-30 15:42:08 +0530

    added method to check if url has video or image and returned proper url to download, whether it is video/image. and added method to get the Mimetype of the content and send accordingly the file

--- DownloadMethods.py
+++ DownloadMethods.py
@@ -4,7 +4,7 @@ import datetime as time
 from bs4 import BeautifulSoup
 
 def GetFileExtension(URL):
-  formats = ['.jpg','.jpeg','.png','.mp3']
+  formats = ['.jpg','.jpeg','.png','.mp3','.mp4']
   for i in formats:
     if (i in URL):
       return i
@@ -14,20 +14,26 @@ def GetFileName(url):
     return 'Download_' + str(time.datetime.now().hour) + str(time.datetime.now().second) + extension
 
 def DownloadPic(url):
-    imgDict = {}
-    htmlText = ""
+#this method checks if the given url has gif, if so return the video/gif url 
     try:
         htmlText = urllib2.urlopen(url)
     except Exception as e:
-        #print e #error handled
         return
     soup = BeautifulSoup(htmlText.read())
+    imgDict = {'picURL':''}
     for meta in soup.find_all('meta'):
+        # then get the video link only
         _property = str(meta.get('property'))
-        if _property == 'og:image':
-            picUrl = str(meta.get('content'))
-            imgDict = {'picURL': picUrl}
-            return imgDict
+        if _property == 'og:video':
+            videoDict = {'picURL': meta.get('content')}
+            return videoDict
+        else:
+            # then get the image content
+            _property = str(meta.get('property'))
+            if _property == 'og:image':
+                picUrl = str(meta.get('content'))
+                imgDict = {'picURL': picUrl}
+    return imgDict
 
 def CreateDownload(url):
     try:
@@ -37,3 +43,17 @@ def CreateDownload(url):
         #print e #error handled
         return 
 
+def GetMimeType(url):
+# accept the filename and detect what is mimetype and send accordingly
+    formats = GetFileExtension(url)
+    return {
+            '.jpg'  :'image/jpeg',
+            '.jpeg' :'image/jpeg',
+            '.png'  :'image/jpeg',
+            '.mp3'  :'audio/mpeg3',
+            '.mp4'  :'video/mp4'
+            }[formats]
+    
+
+#url_video = 'https://www.instagram.com/p/_6QQJujJ9Z/?taken-by=9gag'
+#url_image = 'https://www.instagram.com/p/_6CzIljJ7e/?taken-by=9gag'
