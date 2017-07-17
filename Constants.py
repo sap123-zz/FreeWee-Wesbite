3d7f21a57f69cbcf1aa7b743ddef3dd8c72871bb
Suraj Palwe <surajpalwe13@gmail.com>
2017-07-17 15:20:32 +0530

    Bug fixed : wrong end point of soudcloud download url

--- Constants.py
+++ Constants.py
@@ -3,5 +3,5 @@ end_url   = '.html'
 all_letters  = 'abcdefghijklmnopqrstuvwxyz'
 songsMp3DomainUrl = 'https://www.songsmp3.co'
 soundCloudURL	= 'https://api.soundcloud.com'
-soundCloudEndUrl = '/stream?client_id=eef823eb72081eccc8684bc619021062'
+soundCloudEndUrl = '?client_id=eef823eb72081eccc8684bc619021062'
 
